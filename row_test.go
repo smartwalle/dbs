27bc39f4f01bd80e2cf99a6da371804a1d7b6739
SmartWalle <smartwalle@gmail.com>
2016-07-22 09:27:16 +0800

    添加测试

---
+++ row_test.go
@@ -0,0 +1,53 @@
+package dba
+
+import (
+	"testing"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"fmt"
+	"time"
+)
+
+type Human struct {
+	Id       int       `sql:"id"`
+	Name     string    `sql:"name"`
+	Gender   int       `sql:"gender"`
+	Birthday time.Time `sql:"birthday"`
+	Type     Type
+}
+
+type Type struct {
+	Id   int    `sql:"tid"`
+	Name string `sql:"tname"`
+}
+
+func TestBind(t *testing.T) {
+	var db, err = sql.Open("mysql", "root:smok2015@tcp(192.168.192.250:3306)/test?parseTime=true")
+	if err != nil {
+		fmt.Println("连接数据库出错:", err)
+		return
+	}
+
+	stmt, err := db.Prepare("SELECT h.id, h.name, h.gender, h.birthday, t.id as tid, t.name as tname from human as h LEFT JOIN h_type as t ON h.type=t.id")
+	if err != nil {
+		fmt.Println("Prepare", err)
+		return
+	}
+
+	rows, err := stmt.Query()
+	if err != nil {
+		fmt.Println("Query", err)
+		return
+	}
+
+	var hList []*Human
+	err = Bind(rows, &hList)
+	if err != nil {
+		fmt.Println("Bind", err)
+		return
+	}
+
+	for _, h := range hList {
+		fmt.Println(h.Id, h.Name, h.Gender, h.Birthday, h.Type.Name, h.Type.Id)
+	}
+}
