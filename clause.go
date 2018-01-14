b37b3389bb7194a7bd579ee4272ac41fa4f6ec15
Yangfeng <smartwalle@gmail.com>
2018-01-14 13:48:48 +0800

    update clause

--- clause.go
+++ clause.go
@@ -33,6 +33,10 @@ func NewClause(sql string, args ...interface{}) Clauser {
 	return c
 }
 
+func C(sql string, args ...interface{}) Clauser {
+	return NewClause(sql, args...)
+}
+
 func (this *Clause) AppendToSQL(w io.Writer, sep string, args *Args) {
 	if len(this.sql) > 0 {
 		io.WriteString(w, this.sql)
@@ -116,3 +120,116 @@ func (this Sets) ToSQL(sep string) (string, []interface{}) {
 	this.AppendToSQL(sqlBuffer, sep, args)
 	return sqlBuffer.String(), args.args
 }
+
+// --------------------------------------------------------------------------------
+type where struct {
+	sql      string
+	prefix   string
+	args     []interface{}
+	children []Clauser
+}
+
+func NewWhere(sql string, args ...interface{}) *where {
+	var w = &where{}
+	w.sql = sql
+	w.args = args
+	return w
+}
+
+func (this *where) AppendToSQL(w io.Writer, sep string, args *Args) {
+	var hasSQL = len(this.sql) > 0
+	var hasChildren = len(this.children) > 0
+
+	if len(this.args) > 0 {
+		args.Append(this.args...)
+	}
+
+	if hasSQL || hasChildren {
+		if hasChildren {
+			io.WriteString(w, "(")
+		}
+
+		io.WriteString(w, this.sql)
+
+		for i, e := range this.children {
+			if i != 0 {
+				if len(this.prefix) > 0 {
+					io.WriteString(w, " ")
+					io.WriteString(w, this.prefix)
+					io.WriteString(w, " ")
+				}
+			}
+			e.AppendToSQL(w, sep, args)
+		}
+
+		if hasChildren {
+			io.WriteString(w, ")")
+		}
+	}
+}
+
+func (this *where) ToSQL(sep string) (string, []interface{}) {
+	var sqlBuffer = &bytes.Buffer{}
+	var args = &Args{}
+	this.AppendToSQL(sqlBuffer, " ", args)
+	return sqlBuffer.String(), args.args
+}
+
+//func (this *where) And(cs ...*where) *where {
+//	var w = NewWhere("", nil)
+//	w.children = cs
+//	return this
+//
+//	//var w *where
+//	//for i, c := range cs {
+//	//	//if i == 0 {
+//	//		switch t := c.(type) {
+//	//		case string:
+//	//			if i == 0 {
+//	//				w = NewWhere(t, nil)
+//	//				w.prefix = "AND"
+//	//				this.children = append(this.children, w)
+//	//			} else {
+//	//				w.args = append(w.args, t)
+//	//			}
+//	//		case *where:
+//	//			t.prefix = "AND"
+//	//			this.children = append(this.children, t)
+//	//		case Clauser:
+//	//			this.children = append(this.children, t)
+//	//		default:
+//	//			w.args = append(w.args, t)
+//	//		}
+//	//	//}
+//	//}
+//	//return this
+//}
+//
+//func (this *where) Or(sql string, args ...interface{}) *where {
+//	var w = NewWhere(sql, args...)
+//	w.prefix = "OR"
+//	this.children = append(this.children, w)
+//	return this
+//}
+
+// --------------------------------------------------------------------------------
+func And(cs ...Clauser) *where {
+	var w = &where{}
+	w.children = cs
+	w.prefix = "AND"
+	return w
+}
+
+func Or(cs ...Clauser) *where {
+	var w = &where{}
+	w.children = cs
+	w.prefix = "OR"
+	return w
+}
+//
+//func Not(cs ...Clauser) *where {
+//	var w = &where{}
+//	w.children = cs
+//	w.prefix = "NOT"
+//	return w
+//}
--- clause_test.go
+++ clause_test.go
@@ -3,23 +3,37 @@ package dbs
 import (
 	"testing"
 	"fmt"
+
 )
 
 func TestClause(t *testing.T) {
-	var c1 = NewClause("a=?", 1)
-	var c2 = NewClause("b=?", 2)
-
-	var cs = Clauses{}
-	cs = append(cs, c1)
-	cs = append(cs, c2)
-	fmt.Println(cs.ToSQL(" "))
+	//var c1 = NewClause("a=?", 1)
+	//var c2 = NewClause("b=?", 2)
+	//var cs = Clauses{}
+	//cs = append(cs, c1)
+	//cs = append(cs, c2)
+	//fmt.Println(cs.ToSQL(" "))
 }
 
 func TestSet(t *testing.T) {
-	var s1 = NewSet("sa", 10)
-	var s2 = NewSet("sb", NewClause("sb+?", 11))
-	var ss = Sets{}
-	ss = append(ss, s1)
-	ss = append(ss, s2)
-	fmt.Println(ss.ToSQL(", "))
+	//var s1 = NewSet("sa", 10)
+	//var s2 = NewSet("sb", NewClause("sb+?", 11))
+	//var ss = Sets{}
+	//ss = append(ss, s1)
+	//ss = append(ss, s2)
+	//fmt.Println(ss.ToSQL(", "))
+}
+
+func TestWhere(t *testing.T) {
+	//var w1 = NewWhere("w=?", 10).And("w2=? AND w22=?", 11, 112).Or("w3=?", 12)
+	//fmt.Println(w1.ToSQL(""))
+
+	var w2 = And(NewClause("w1=?", 10), Or(NewClause("w2=?", 20), NewClause("w3=?", 30)))
+
+	//w2.And(NewWhere("w2=?", 20), NewWhere("w3=?", 30))
+	fmt.Println(w2.ToSQL(""))
+
+
+	//var wa = Or(And(C("w1=?", 10), C("w2=?", 20)), And(C("w3=?", 30), C("w4=?", 40)), Not(C("w5")))
+	//fmt.Println(wa.ToSQL(""))
 }
