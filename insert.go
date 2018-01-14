2dbe19ade5f315987741251df7121b0614e02163
Yangfeng <smartwalle@gmail.com>
2018-01-14 16:38:12 +0800

    update

---
+++ insert.go
@@ -0,0 +1,155 @@
+package dbs
+
+import (
+	"errors"
+	"bytes"
+	"strings"
+	"fmt"
+	"database/sql"
+)
+
+type InsertBuilder struct {
+	prefixes Clauses
+	options  Clauses
+	columns  []string
+	table    string
+	values   [][]interface{}
+	suffixes Clauses
+}
+
+func (this *InsertBuilder) Prefix(sql string, args ...interface{}) *InsertBuilder {
+	this.prefixes = append(this.prefixes, NewClause(sql, args...))
+	return this
+}
+
+func (this *InsertBuilder) Options(options ...string) *InsertBuilder {
+	for _, c := range options {
+		this.options = append(this.options, NewClause(c))
+	}
+	return this
+}
+
+func (this *InsertBuilder) Columns(columns ...string) *InsertBuilder {
+	this.columns = append(this.columns, columns...)
+	return this
+}
+
+func (this *InsertBuilder) Column(column string) *InsertBuilder {
+	this.columns = append(this.columns, column)
+	return this
+}
+
+func (this *InsertBuilder) Table(table string) *InsertBuilder {
+	this.table = table
+	return this
+}
+
+func (this *InsertBuilder) Values(values ...interface{}) *InsertBuilder {
+	this.values = append(this.values, values)
+	return this
+}
+
+func (this *InsertBuilder) Suffix(sql string, args ...interface{}) *InsertBuilder {
+	this.suffixes = append(this.suffixes, NewClause(sql, args...))
+	return this
+}
+
+func (this *InsertBuilder) SET(column string, value interface{}) *InsertBuilder {
+	this.columns = append(this.columns, column)
+	if len(this.values) == 0 {
+		this.values = append(this.values, make([]interface{}, 0, 0))
+	}
+	var vList = this.values[0]
+	vList = append(vList, value)
+	this.values[0] = vList
+	return this
+}
+
+func (this *InsertBuilder) ToSQL() (string, []interface{}, error) {
+	var args = NewArgs()
+	var err error
+	if len(this.table) == 0 {
+		err = errors.New("insert statements must specify a table")
+		return "", nil, err
+	}
+	if len(this.values) == 0 {
+		err = errors.New("insert statements must have at least one set of values")
+		return "", nil, err
+	}
+
+	var sqlBuffer = &bytes.Buffer{}
+
+	if len(this.prefixes) > 0 {
+		this.prefixes.AppendToSQL(sqlBuffer, " ", args)
+		sqlBuffer.WriteString(" ")
+	}
+
+	sqlBuffer.WriteString("INSERT ")
+
+	if len(this.options) > 0 {
+		this.options.AppendToSQL(sqlBuffer, " ", args)
+		sqlBuffer.WriteString(" ")
+	}
+
+	sqlBuffer.WriteString("INTO `")
+	sqlBuffer.WriteString(this.table)
+	sqlBuffer.WriteString("` ")
+
+	if len(this.columns) > 0 {
+		sqlBuffer.WriteString("(`")
+		sqlBuffer.WriteString(strings.Join(this.columns, "`, `"))
+		sqlBuffer.WriteString("`)")
+	}
+
+	sqlBuffer.WriteString(" VALUES ")
+
+	var valuesPlaceholder = make([]string, len(this.values))
+	for index, value := range this.values {
+		var valuePlaceholder = make([]string, len(value))
+		for i, v := range value {
+			switch vt := v.(type) {
+			case Clauser:
+				vSQL, vArgs := vt.ToSQL()
+				valuePlaceholder[i] = vSQL
+				args.Append(vArgs...)
+			default:
+				valuePlaceholder[i] = "?"
+				args.Append(v)
+			}
+		}
+		valuesPlaceholder[index] = fmt.Sprintf("(%s)", strings.Join(valuePlaceholder, ", "))
+	}
+	sqlBuffer.WriteString(strings.Join(valuesPlaceholder, ", "))
+
+	if len(this.suffixes) > 0 {
+		sqlBuffer.WriteString(" ")
+		this.suffixes.AppendToSQL(sqlBuffer, " ", args)
+	}
+
+	return sqlBuffer.String(), args.values, err
+}
+
+func (this *InsertBuilder) Exec(s SQLExecutor) (sql.Result, error) {
+	sql, args, err := this.ToSQL()
+	if err != nil {
+		return nil, err
+	}
+	return Exec(s, sql, args...)
+}
+
+func Insert(s SQLExecutor, table string, data map[string]interface{}) (sql.Result, error) {
+	var in = NewInsertBuilder()
+	in.Table(table)
+
+	var values []interface{}
+	for k, v := range data {
+		in.Column(k)
+		values = append(values, v)
+	}
+	in.Values(values...)
+	return in.Exec(s)
+}
+
+func NewInsertBuilder() *InsertBuilder {
+	return &InsertBuilder{}
+}
---
+++ insert_test.go
@@ -0,0 +1,15 @@
+package dbs
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestInsertBuilder_ToSQL(t *testing.T) {
+	var ib = NewInsertBuilder()
+	ib.Table("user")
+	ib.SET("name", "yang")
+	ib.SET("email", "yang@qq.com")
+	ib.SET("amount", NewClause("((SELECT `amount` FROM `class` WHERE id=? LIMIT 1)+?)", 10))
+	fmt.Println(ib.ToSQL())
+}
