7c5bc19b5e44079bdfd078ca3b3236ec96cbb65f
SmartWalle <smartwalle@gmail.com>
2018-05-18 13:21:04 +0800

    调整 Tx 和 Stmt 缓存

--- builder_test.go
+++ builder_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestBuilder(t *testing.T) {
 	fmt.Println("===== RawBuilder =====")
-	var b = NewBuilder()
+	var b = NewBuilder("")
 	b.Append("SELECT a.id, a.name")
 	b.Format("FROM %s AS a", "add")
 	b.Append("WHERE id>?", 10)
