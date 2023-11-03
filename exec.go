6bd9172222884449b7382606bf2fa6b5d4db3ebf
smartwalle <smartwalle@gmail.com>
2023-11-03 12:43:16 +0800

    清理

--- exec.go
+++ exec.go
@@ -24,153 +24,58 @@ func Exec(ctx context.Context, session Session, query string, args ...interface{
 	return session.ExecContext(ctx, query, args...)
 }
 
-func scanContext(ctx context.Context, s Session, b Builder, dst interface{}) (err error) {
-	//var tx Transaction
-	//var prefix string
-	//
-	//if nTx, ok := s.(Transaction); ok {
-	//	tx = nTx
-	//	prefix = tx.String() + " " + b.Type()
-	//} else {
-	//	prefix = b.Type()
-	//}
-	//
-	//defer func() {
-	//	if err != nil && tx != nil {
-	//		tx.rollback(5)
-	//	}
-	//}()
-
-	sqlStr, args, err := b.SQL()
+func scanContext(ctx context.Context, session Session, builder Builder, dst interface{}) error {
+	query, args, err := builder.SQL()
 	if err != nil {
-		//logger.Output(3, fmt.Sprintln(prefix, "Build Failed:", err))
 		return err
 	}
-	//logger.Output(3, fmt.Sprintln(prefix, "Build Success:", sqlStr, args))
-	rows, err := s.QueryContext(ctx, sqlStr, args...)
+	rows, err := session.QueryContext(ctx, query, args...)
 	if err != nil {
-		//logger.Output(3, fmt.Sprintln(prefix, "Query Failed:", err))
 		return err
 	}
 	defer rows.Close()
 
 	if err = mapper.Decode(rows, dst); err != nil {
-		//logger.Output(3, fmt.Sprintln(prefix, "Scan Failed:", err))
 		return err
 	}
 	return nil
 }
 
-func scanRowContext(ctx context.Context, s Session, b Builder, dst ...interface{}) (err error) {
-	//var tx Transaction
-	//var prefix string
-	//
-	//if nTx, ok := s.(Transaction); ok {
-	//	tx = nTx
-	//	prefix = tx.String() + " " + b.Type()
-	//} else {
-	//	prefix = b.Type()
-	//}
-	//
-	//defer func() {
-	//	if err != nil && tx != nil {
-	//		tx.rollback(5)
-	//	}
-	//}()
-
-	sqlStr, args, err := b.SQL()
+func scanRowContext(ctx context.Context, session Session, builder Builder, dst ...interface{}) error {
+	query, args, err := builder.SQL()
 	if err != nil {
-		//logger.Output(3, fmt.Sprintln(prefix, "Build Failed:", err))
 		return err
 	}
-	//logger.Output(3, fmt.Sprintln(prefix, "Build Success:", sqlStr, args))
-	rows, err := s.QueryContext(ctx, sqlStr, args...)
+	rows, err := session.QueryContext(ctx, query, args...)
 	if err != nil {
-		//logger.Output(3, fmt.Sprintln(prefix, "Query Failed:", err))
 		return err
 	}
 	defer rows.Close()
 
-	//for _, dp := range dst {
-	//	if _, ok := dp.(*sql.RawBytes); ok {
-	//		err = errors.New("sql: RawBytes isn't allowed on Row.Scan")
-	//		logger.Output(3, fmt.Sprintln(prefix, "Scan Failed:", err))
-	//		return err
-	//	}
-	//}
-
 	if !rows.Next() {
 		if err = rows.Err(); err != nil {
-			//logger.Output(3, fmt.Sprintln(prefix, "Scan Failed:", err))
 			return err
 		}
-		//logger.Output(3, fmt.Sprintln(prefix, "Scan Failed:", sql.ErrNoRows))
 		return sql.ErrNoRows
 	}
 	if err = rows.Scan(dst...); err != nil {
-		//logger.Output(3, fmt.Sprintln(prefix, "Scan Failed:", err))
 		return err
 	}
-	return rows.Close()
+	return nil
 }
 
-func queryContext(ctx context.Context, s Session, b Builder) (result *sql.Rows, err error) {
-	//var tx Transaction
-	//var prefix string
-	//
-	//if nTx, ok := s.(Transaction); ok {
-	//	tx = nTx
-	//	prefix = tx.String() + " " + b.Type()
-	//} else {
-	//	prefix = b.Type()
-	//}
-	//
-	//defer func() {
-	//	if err != nil && tx != nil {
-	//		tx.rollback(5)
-	//	}
-	//}()
-
-	sqlStr, args, err := b.SQL()
+func queryContext(ctx context.Context, s Session, b Builder) (*sql.Rows, error) {
+	query, args, err := b.SQL()
 	if err != nil {
-		//logger.Output(3, fmt.Sprintln(prefix, "Build Failed:", err))
 		return nil, err
 	}
-	//logger.Output(3, fmt.Sprintln(prefix, "Build Success:", sqlStr, args))
-	result, err = s.QueryContext(ctx, sqlStr, args...)
-	//if err != nil {
-	//	logger.Output(3, fmt.Sprintln(prefix, "Query Failed:", err))
-	//}
-	return result, err
+	return s.QueryContext(ctx, query, args...)
 }
 
-func execContext(ctx context.Context, s Session, b Builder) (result sql.Result, err error) {
-	//var tx Transaction
-	//var prefix string
-	//
-	//if nTx, ok := s.(Transaction); ok {
-	//	tx = nTx
-	//	prefix = tx.String() + " " + b.Type()
-	//} else {
-	//	prefix = b.Type()
-	//}
-	//
-	//defer func() {
-	//	if err != nil && tx != nil {
-	//		tx.rollback(5)
-	//	}
-	//}()
-
-	sqlStr, args, err := b.SQL()
+func execContext(ctx context.Context, session Session, builder Builder) (sql.Result, error) {
+	query, args, err := builder.SQL()
 	if err != nil {
-		//logger.Output(3, fmt.Sprintln(prefix, "Build Failed:", err))
 		return nil, err
 	}
-
-	//logger.Output(3, fmt.Sprintln(prefix, "Build Success:", sqlStr, args))
-	result, err = s.ExecContext(ctx, sqlStr, args...)
-	//if err != nil {
-	//	logger.Output(3, fmt.Sprintln(prefix, "Exec Failed:", err))
-	//}
-	return result, err
+	return session.ExecContext(ctx, query, args...)
 }
