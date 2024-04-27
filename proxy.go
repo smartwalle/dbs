587602351cf97566cd1c9b785e8fb0eff87046fa
SmartWalle <smartwalle@gmail.com>
2024-04-27 09:57:04 +0800

    proxy and shard

---
+++ proxy.go
@@ -0,0 +1,109 @@
+package dbs
+
+import (
+	"context"
+	"database/sql"
+	"sync/atomic"
+)
+
+// Proxy 维护一个 master 节点和多个 slave 节点的数据库连接信息，用于客户端实现读写分离操作。
+//
+// 执行写（默认为调用 DB.Exec()、Proxy.ExecContext() 方法执行的操作）的操作在 master 节点上执行。
+//
+// 执行读（默认为调用 Proxy.Query()、DB.QueryContext()、Proxy.QueryRow()、DB.QueryRowContext() 方法执行的操作）的操作默认在 slave 节点上执行。
+type Proxy struct {
+	master         Database
+	slaves         []Database
+	numberOfSlaves int
+	slaveOffset    uint32
+}
+
+type masterKey struct{}
+
+// WithMaster 有一些特殊情况需要在 master 节点执行 Query 相关的操作，可以通过传递 WithMaster() 函数返回的 context.Context
+// 用于指定该操作在 master 节点上执行。
+//
+// 仅 Proxy.QueryContext() 和 DB.QueryRowContext() 支持。
+func WithMaster(ctx context.Context) context.Context {
+	return context.WithValue(ctx, masterKey{}, true)
+}
+
+type slaveKey struct{}
+
+// WithSlave 用于指定操作在 slave 节点上执行。
+//
+// 仅 Proxy.PrepareContext() 支持。
+func WithSlave(ctx context.Context) context.Context {
+	return context.WithValue(ctx, slaveKey{}, true)
+}
+
+func NewProxy(master Database, slaves ...Database) *Proxy {
+	var ndb = &Proxy{}
+	ndb.master = master
+	ndb.slaves = slaves
+	ndb.numberOfSlaves = len(slaves)
+	return ndb
+}
+
+func (p *Proxy) Prepare(query string) (*sql.Stmt, error) {
+	return p.PrepareContext(context.Background(), query)
+}
+
+func (p *Proxy) PrepareContext(ctx context.Context, query string) (*sql.Stmt, error) {
+	var slave, _ = ctx.Value(slaveKey{}).(bool)
+	if slave && p.numberOfSlaves > 0 {
+		var node = p.slaves[int(atomic.AddUint32(&p.slaveOffset, 1)-1)%p.numberOfSlaves]
+		return node.PrepareContext(ctx, query)
+	}
+	return p.master.PrepareContext(ctx, query)
+}
+
+func (p *Proxy) Exec(query string, args ...interface{}) (sql.Result, error) {
+	return p.ExecContext(context.Background(), query, args...)
+}
+
+func (p *Proxy) ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
+	return p.master.ExecContext(ctx, query, args...)
+}
+
+func (p *Proxy) Query(query string, args ...interface{}) (*sql.Rows, error) {
+	return p.QueryContext(context.Background(), query, args...)
+}
+
+func (p *Proxy) QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error) {
+	var master, _ = ctx.Value(masterKey{}).(bool)
+	if !master && p.numberOfSlaves > 0 {
+		var node = p.slaves[int(atomic.AddUint32(&p.slaveOffset, 1)-1)%p.numberOfSlaves]
+		return node.QueryContext(ctx, query, args...)
+	}
+	return p.master.QueryContext(ctx, query, args...)
+}
+
+func (p *Proxy) QueryRow(query string, args ...any) *sql.Row {
+	return p.QueryRowContext(context.Background(), query, args...)
+}
+
+func (p *Proxy) QueryRowContext(ctx context.Context, query string, args ...any) *sql.Row {
+	var master, _ = ctx.Value(masterKey{}).(bool)
+	if !master && p.numberOfSlaves > 0 {
+		var node = p.slaves[int(atomic.AddUint32(&p.slaveOffset, 1)-1)%p.numberOfSlaves]
+		return node.QueryRowContext(ctx, query, args...)
+	}
+	return p.master.QueryRowContext(ctx, query, args...)
+}
+
+func (p *Proxy) Close() error {
+	p.master.Close()
+	for _, slave := range p.slaves {
+		slave.Close()
+	}
+	return nil
+}
+
+func (p *Proxy) Begin() (*Tx, error) {
+	return p.master.Begin()
+}
+
+func (p *Proxy) BeginTx(ctx context.Context, opts *sql.TxOptions) (*Tx, error) {
+	return p.master.BeginTx(ctx, opts)
+}
---
+++ shard.go
@@ -0,0 +1,81 @@
+package dbs
+
+import (
+	"context"
+	"database/sql"
+)
+
+// Shard 维护一组数据库连接信息，用于客户端实现数据库层面的分片操作。
+type Shard struct {
+	sharding       func(value string) int
+	shards         []Database
+	numberOfShards int
+}
+
+type shardKey struct{}
+
+// WithShardValue 设定一个值，该值会传递到 sharding 函数中，用于分片节点选举计算。
+func WithShardValue(ctx context.Context, value string) context.Context {
+	return context.WithValue(ctx, shardKey{}, value)
+}
+
+func NewShard(sharding func(value string) int, shards ...Database) *Shard {
+	var ndb = &Shard{}
+	ndb.sharding = sharding
+	ndb.shards = shards
+	ndb.numberOfShards = len(shards)
+	return ndb
+}
+
+func (s *Shard) shard(ctx context.Context) Database {
+	var value, _ = ctx.Value(shardKey{}).(string)
+	var idx = s.sharding(value)
+	return s.shards[idx]
+}
+
+func (s *Shard) Prepare(query string) (*sql.Stmt, error) {
+	return s.PrepareContext(context.Background(), query)
+}
+
+func (s *Shard) PrepareContext(ctx context.Context, query string) (*sql.Stmt, error) {
+	return s.shard(ctx).PrepareContext(ctx, query)
+}
+
+func (s *Shard) Exec(query string, args ...interface{}) (sql.Result, error) {
+	return s.ExecContext(context.Background(), query, args...)
+}
+
+func (s *Shard) ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
+	return s.shard(ctx).ExecContext(ctx, query, args...)
+}
+
+func (s *Shard) Query(query string, args ...interface{}) (*sql.Rows, error) {
+	return s.QueryContext(context.Background(), query, args...)
+}
+
+func (s *Shard) QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error) {
+	return s.shard(ctx).QueryContext(ctx, query, args...)
+}
+
+func (s *Shard) QueryRow(query string, args ...any) *sql.Row {
+	return s.QueryRowContext(context.Background(), query, args...)
+}
+
+func (s *Shard) QueryRowContext(ctx context.Context, query string, args ...any) *sql.Row {
+	return s.shard(ctx).QueryRowContext(ctx, query, args...)
+}
+
+func (s *Shard) Close() error {
+	for _, db := range s.shards {
+		db.Close()
+	}
+	return nil
+}
+
+func (s *Shard) Begin() (*Tx, error) {
+	return s.BeginTx(context.Background(), nil)
+}
+
+func (s *Shard) BeginTx(ctx context.Context, opts *sql.TxOptions) (*Tx, error) {
+	return s.shard(ctx).BeginTx(ctx, opts)
+}
