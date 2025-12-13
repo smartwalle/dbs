81e48db437bf1ae2fba501a04800def286f79e14
SmartWalle <smartwalle@gmail.com>
2025-12-13 11:47:44 +0800

    Repository

--- repository.go
+++ repository.go
@@ -40,51 +40,29 @@ func (r *Repository[E]) Insert(ctx context.Context, entity *E) (sql.Result, erro
 		values[idx] = fieldValue.Value
 	}
 
-	var ib = NewInsertBuilder()
-	ib.UseDialect(r.dialect)
-	ib.UseSession(r.db.Session(ctx))
-
-	ib.Table(r.entity.TableName())
+	var ib = r.InsertBuilder(ctx)
 	ib.Columns(columns...)
 	ib.Values(values...)
-
 	return ib.Exec(ctx)
 }
 
 func (r *Repository[E]) Delete(ctx context.Context, id interface{}) (sql.Result, error) {
-	var rb = NewDeleteBuilder()
-	rb.UseDialect(r.dialect)
-	rb.UseSession(r.db.Session(ctx))
-
-	rb.Table(r.entity.TableName())
+	var rb = r.DeleteBuilder(ctx)
 	rb.Where(r.entity.PrimaryKey()+" = ?", id)
-
 	return rb.Exec(ctx)
 }
 
 func (r *Repository[E]) Update(ctx context.Context, id interface{}, values map[string]interface{}) (sql.Result, error) {
-	var ub = NewUpdateBuilder()
-	ub.UseDialect(r.dialect)
-	ub.UseSession(r.db.Session(ctx))
-
-	ub.Table(r.entity.TableName())
+	var ub = r.UpdateBuilder(ctx)
 	ub.SetValues(values)
 	ub.Where(r.entity.PrimaryKey()+" = ?", id)
-
 	return ub.Exec(ctx)
 }
 
-func (r *Repository[E]) Select(ctx context.Context, id interface{}, columns ...string) (*E, error) {
-	var sb = NewSelectBuilder()
-	sb.UseDialect(r.dialect)
-	sb.UseSession(r.db.Session(ctx))
-
-	sb.Table(r.entity.TableName())
-	if len(columns) > 0 {
-		sb.Selects(columns...)
-	} else {
-		sb.Selects("*")
-	}
+func (r *Repository[E]) Find(ctx context.Context, id interface{}, columns string) (*E, error) {
+	var sb = r.SelectBuilder(ctx)
+	sb.Selects(columns)
+	sb.Limit(1)
 	sb.Where(r.entity.PrimaryKey()+" = ?", id)
 
 	var entity *E
@@ -92,4 +70,91 @@ func (r *Repository[E]) Select(ctx context.Context, id interface{}, columns ...s
 		return nil, err
 	}
 	return entity, nil
+}
+
+func (r *Repository[E]) FindOne(ctx context.Context, columns string, conds string, args ...interface{}) (*E, error) {
+	var sb = r.SelectBuilder(ctx)
+	sb.Selects(columns)
+	sb.Limit(1)
+	sb.Where(conds, args...)
+
+	var entity *E
+	if err := sb.Scan(ctx, &entity); err != nil {
+		return nil, err
+	}
+	return entity, nil
+}
+
+func (r *Repository[E]) FindList(ctx context.Context, columns string, conds string, args ...interface{}) ([]*E, error) {
+	var sb = r.SelectBuilder(ctx)
+	sb.Selects(columns)
+	sb.Where(conds, args...)
+
+	var entityList []*E
+	if err := sb.Scan(ctx, &entityList); err != nil {
+		return nil, err
+	}
+	return entityList, nil
+}
+
+func (r *Repository[E]) InsertBuilder(ctx context.Context) *InsertBuilder {
+	var ib = NewInsertBuilder()
+	ib.UseDialect(r.dialect)
+	ib.UseSession(r.db.Session(ctx))
+
+	ib.Table(r.entity.TableName())
+	return ib
+}
+
+func (r *Repository[E]) DeleteBuilder(ctx context.Context) *DeleteBuilder {
+	var rb = NewDeleteBuilder()
+	rb.UseDialect(r.dialect)
+	rb.UseSession(r.db.Session(ctx))
+
+	rb.Table(r.entity.TableName())
+	return rb
+}
+
+func (r *Repository[E]) UpdateBuilder(ctx context.Context) *UpdateBuilder {
+	var ub = NewUpdateBuilder()
+	ub.UseDialect(r.dialect)
+	ub.UseSession(r.db.Session(ctx))
+
+	ub.Table(r.entity.TableName())
+	return ub
+}
+
+func (r *Repository[E]) SelectBuilder(ctx context.Context) *SelectBuilder {
+	var sb = NewSelectBuilder()
+	sb.UseDialect(r.dialect)
+	sb.UseSession(r.db.Session(ctx))
+
+	sb.Table(r.entity.TableName())
+	return sb
+}
+
+func (r *Repository[E]) Transaction(ctx context.Context, fn func(ctx context.Context) error, opts ...*sql.TxOptions) (err error) {
+	var tx = Transaction(ctx)
+	if tx == nil {
+		var opt *sql.TxOptions
+		if len(opts) > 0 {
+			opt = opts[0]
+		}
+		tx, err = r.db.BeginTx(ctx, opt)
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err != nil {
+				tx.Rollback()
+			}
+		}()
+
+		if err = fn(tx.Context(ctx)); err != nil {
+			return err
+		}
+		return tx.Commit()
+	}
+
+	return fn(ctx)
 }
