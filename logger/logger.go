c007c2e5ed1ca5b82bb8e278f8f865703819a887
SmartWalle <smartwalle@gmail.com>
2026-01-27 19:48:56 +0800

    refactor: 优化日志处理逻辑并提取到独立包
    将日志实现移动到独立的logger包，并优化日志调用逻辑，仅在logger非nil时记录日志

---
+++ logger/logger.go
@@ -0,0 +1,27 @@
+package logger
+
+import (
+	"context"
+	"log"
+	"runtime"
+	"time"
+)
+
+func New() *Logger {
+	return &Logger{}
+}
+
+type Logger struct {
+}
+
+func (Logger) Trace(ctx context.Context, skip int, begin time.Time, sql string, args []any, rowsAffected int64, err error) {
+	var elapsedTime = time.Since(begin)
+
+	_, file, line, _ := runtime.Caller(skip)
+
+	if err != nil {
+		log.Printf("File: %s:%d, SQL: %s, Args: %+v, ElapsedTime: %+v, Error: %+v \n", file, line, sql, args, elapsedTime, err)
+	} else {
+		log.Printf("File: %s:%d, SQL: %s, Args: %+v, ElapsedTime: %+v, Rows: %d \n", file, line, sql, args, elapsedTime, rowsAffected)
+	}
+}
