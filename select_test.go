06d95e40bbce9f3f1e067c07986fc475c3536e43
SmartWalle <smartwalle@gmail.com>
2026-09-17 10:27:37 +0800

    test: 添加 Explain 基准测试

--- select_test.go
+++ select_test.go
@@ -39,6 +39,16 @@ func BenchmarkSelectBuilder(b *testing.B) {
 	}
 }
 
+func BenchmarkSelectBuilder_Explain(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var sb = dbs.NewSelectBuilder()
+		sb.Selects("id, user_id, pay_no, trade_no, goods_id, goods_name, goods_price, goods_cnt, sku_id, original_spec, real_spec, status, created_at")
+		sb.Table("order")
+		sb.Where("id = ? AND status = ?", "123", 1)
+		_, _ = sb.Explain()
+	}
+}
+
 var pool = sync.Pool{
 	New: func() any {
 		return dbs.NewSelectBuilder()
